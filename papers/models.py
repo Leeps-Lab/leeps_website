2a58f9df9bcec0afbf289b664c137ad7c38eecd1
jpettit <jpettit@637b740e-b1db-41bf-a134-2c17f54ac410>
2007-11-13 22:07:35 +0000

    git-svn-id: file:///svn/leeps_website@127 637b740e-b1db-41bf-a134-2c17f54ac410

--- papers/models.py
+++ papers/models.py
@@ -10,10 +10,18 @@ class Paper(models.Model):
     paper = models.FileField(blank=True, null=True, upload_to='papers/')
     data = models.FileField(blank=True, null=True, upload_to='papers/data/')
     code = models.FileField(blank=True, null=True, upload_to='papers/code/')
+    publish = models.CharField(max_length=5,
+        choices=(('true', 'Yes'), ('false', 'No')),
+        radio_admin=True,
+        help_text='''If "Yes", all files attached to this paper will be available for download.
+                     If paper has no attached file(s), just put "No"''')
     
     def __str__(self):
         return self.title
+        
+    class Meta:
+        ordering=('-date', 'title')
     
     class Admin:
-        pass
+        list_display=('title', 'date', 'publish')
     
