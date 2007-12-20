27d558af5da9ab85634a0a8d976508cbd7fe488a
jpettit <jpettit@637b740e-b1db-41bf-a134-2c17f54ac410>
2007-12-20 17:31:20 +0000

    git-svn-id: file:///svn/leeps_website@242 637b740e-b1db-41bf-a134-2c17f54ac410

--- runserver.sh
+++ runserver.sh
@@ -1,1 +1,1 @@
-PYTHONPATH=~/code/django/:~/code/Imaging-1.1.6/:~/code/markdown-1.6b ./manage.py runserver 128.114.138.3:8000
+PYTHONPATH=~/code/Imaging-1.1.6/:~/code/markdown-1.6b ./manage.py runserver 128.114.138.3:8000
