e1a22890a1c90b8399745f5b9b0fdbce72cf93d3
root <root@637b740e-b1db-41bf-a134-2c17f54ac410>
2008-04-04 04:56:37 +0000

    git-svn-id: file:///svn/leeps_website@417 637b740e-b1db-41bf-a134-2c17f54ac410

---
+++ urls.py
@@ -0,0 +1,39 @@
+from django.conf.urls.defaults import *
+from django.contrib.sitemaps import GenericSitemap
+from leeps_website.papers.models import Paper
+from leeps_website.people.models import Person
+from leeps_website.projects.models import Project
+from leeps_website.classes.models import Class
+from leeps_website.models import Page
+
+sitemaps = {
+    'papers': GenericSitemap({'queryset': Paper.objects.all()}),
+    'people': GenericSitemap({'queryset': Person.objects.all()}),
+    'projects': GenericSitemap({'queryset': Project.objects.all()}),
+    'classes': GenericSitemap({'queryset': Class.objects.all()}),
+    'pages': GenericSitemap({'queryset': Page.objects.all()}),
+}
+
+urlpatterns = patterns('',
+    # app content
+    (r'^papers/', include('leeps_website.papers.urls')),
+    (r'^people/', include('leeps_website.people.urls')),
+    (r'^projects/', include('leeps_website.projects.urls')),
+    (r'^classes/', include('leeps_website.classes.urls')),
+
+    # admin
+    (r'^admin/', include('django.contrib.admin.urls')),
+
+    (r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps})
+)
+
+urlpatterns += patterns('leeps_website.views',
+    # main content
+    (r'^$|^home/$', 'home'),
+    (r'^about/$', 'about'),
+)
+
+urlpatterns += patterns('django.views.generic.simple',
+    # simple redirect
+    (r'^calendar/$', 'redirect_to', {'url': 'http://econlab.ucsc.edu/public/show_calendar.php'}),
+)
