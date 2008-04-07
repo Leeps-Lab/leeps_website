9c2ce8ef421aea8dfc199ef9e7de9808445d04d5
jpettit <jpettit@637b740e-b1db-41bf-a134-2c17f54ac410>
2008-04-07 07:43:33 +0000

    git-svn-id: file:///svn/leeps_website@425 637b740e-b1db-41bf-a134-2c17f54ac410

--- scriptr/views.py
+++ scriptr/views.py
@@ -32,22 +32,3 @@ def details(request, slug):
               'examples': examples,
               'form': form },
             context_instance=RequestContext(request))
-            
-    if request.method == 'POST':
-        form_class = RunScriptFormFactory(script.get_file_filename())
-        form = form_class(request.POST)
-        if form.is_valid():
-            glob = {}
-            loc = {}
-            execfile(script.get_file_filename(), glob, loc)
-            output_filename = uuid.uuid4().hex+'.png'
-            output_path = os.path.join(
-                    '/opt/local/var/leeps_website/', 'site_media', 'scriptr', output_filename)
-            loc['main_script'](form.cleaned_data, output_path)
-            return HttpResponseRedirect('/site_media/scriptr/'+output_filename)
-    else:
-        form_class = RunScriptFormFactory(script.get_file_filename())
-        form = form_class()
-    return render_to_response('scriptr/run_script.html',
-            { 'form': form },
-            context_instance=RequestContext(request))
