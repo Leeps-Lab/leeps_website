255fc2937dbf98a5ab0a750e9ed925323e215536
jpettit <jpettit@637b740e-b1db-41bf-a134-2c17f54ac410>
2008-07-28 22:20:47 +0000

    Little hack to get around newlines in jnlp xml. Tested starting with arguments, works.
    git-svn-id: file:///svn/leeps_website@679 637b740e-b1db-41bf-a134-2c17f54ac410

--- fire/jnlp.py
+++ fire/jnlp.py
@@ -1,5 +1,32 @@
 import xml.dom.minidom
 
+def fixed_writexml(self, writer, indent="", addindent="", newl=""):
+    writer.write(indent+"<" + self.tagName)
+
+    attrs = self._get_attributes()
+    a_names = attrs.keys()
+    a_names.sort()
+
+    for a_name in a_names:
+        writer.write(" %s=\"" % a_name)
+        xml.dom.minidom._write_data(writer, attrs[a_name].value)
+        writer.write("\"")
+    if self.childNodes:
+        if len(self.childNodes) == 1 \
+          and self.childNodes[0].nodeType == xml.dom.minidom.Node.TEXT_NODE:
+            writer.write(">")
+            self.childNodes[0].writexml(writer, "", "", "")
+            writer.write("</%s>%s" % (self.tagName, newl))
+            return
+        writer.write(">%s"%(newl))
+        for node in self.childNodes:
+            node.writexml(writer,indent+addindent,addindent,newl)
+        writer.write("%s</%s>%s" % (indent,self.tagName,newl))
+    else:
+        writer.write("/>%s"%(newl))
+# replace minidom's function with ours
+xml.dom.minidom.Element.writexml = fixed_writexml
+
 def new_argument(doc, arg):
     argument = doc.createElement("argument")
     argument.appendChild(doc.createTextNode(arg))
@@ -42,10 +69,6 @@ def create_jnlp(username, password, server, kindling, configuration):
     jar = doc.createElement("jar")
     jar.setAttribute("href", "FIRE-Libraries.jar")
     resources.appendChild(jar)
-
-    jnlp.appendChild(resources)
-
-    application_desc = doc.createElement("application-desc")
 
     jnlp.appendChild(resources)
 
