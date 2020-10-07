187b1b678acc138ba7ecb739df49f142b4e2358a
Morgan Grant <gramorgan@gmail.com>
2020-10-07 11:08:51 -0700

    fix bug with readings get_readings_by_tag

--- leeps_website/classes/views.py
+++ leeps_website/classes/views.py
@@ -25,16 +25,19 @@ def details(request, class_name):
 def get_readings_by_tag(request, class_name, tag_name):
     cls = get_object_or_404(Class, slug=class_name)
     readings = Reading.objects.filter(cls=cls)
-    tags = {}
-    for reading in readings:
-        tags[reading.tag] = ""
-    if None in tags:
-        del tags[None]
+    tags = [reading.tag for reading in readings if reading.tag is not None]
+    tags.insert(0, 'All')
     tag_name = str(tag_name)
     if tag_name.count('All') == 1:
         tagged = readings
     else:
         tagged = [reading for reading in readings if tag_name.count(str(reading.tag)) == 1]
-    return render(request,
-            'classes/details.html',
-            { 'class': cls, 'readings': tagged, 'tags': ['All']+tags.keys()})
+    return render(
+        request,
+        'classes/details.html',
+        {
+            'class': cls,
+            'readings': tagged,
+            'tags': tags,
+        }
+    )
